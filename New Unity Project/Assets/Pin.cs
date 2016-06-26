5a4354b8f9662751bf032987fd891d800c7db2cb
Xavier <xroba@rubbik.com>
2016-06-27 00:09:20 +0200

    swiperBox

---
+++ New Unity Project/Assets/Pin.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pin : MonoBehaviour {
+	private Vector3 euler;
+	public float angleLimitThreshold = 3f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		isStanding();
+	}
+
+	public bool isStanding(){
+
+		float tiltx = Mathf.Abs(transform.eulerAngles.x);
+		float tilty = Mathf.Abs(transform.eulerAngles.y);
+
+		if( (tiltx > angleLimitThreshold) && (tilty > angleLimitThreshold)){
+			return false;
+		}
+		return true;
+	}
+}
---
+++ New Unity Project/Assets/PinSetter.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PinSetter : MonoBehaviour {
+
+	Pin[] ArrPins;
+	int numberOfPin;
+	public GameObject UILeftPanelCount;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//allways count pin standing
+
+		numberOfPin = CountNumberOfStandingPins();
+
+		//update left Panel Text
+		UILeftPanelCount.GetComponent<Text>().text = numberOfPin.ToString();
+
+	}
+
+	private int CountNumberOfStandingPins(){
+
+		//retrieve allPins
+		ArrPins = retrieveAllPin();
+
+		numberOfPin = 0;
+		foreach(Pin mpin in ArrPins){
+			if(mpin.isStanding()){
+				numberOfPin++;
+			}
+
+		}
+
+		return numberOfPin;
+	}
+
+	private Pin[] retrieveAllPin(){
+		return FindObjectsOfType<Pin>();
+	}
+
+
+}
